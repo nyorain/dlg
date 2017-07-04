cd7df43b3efb94f4fa953b7a89ee91f16d6c7895
nyorain <nyorain@gmail.com>
2017-07-04 14:34:50 +0200

    Add custom macro to sample

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Not real windows support yet.
 To do
 =====
 
-- [ ] Add field to Origin that determines whether the origin is inside a dlg::check block?
-- [ ] Rework/further strip fmt.hpp
-	- [ ] Since it is parsed to some type-erased list anyways, don't include the whole header
+- [ ] add field to Origin that determines whether the origin is inside a dlg::check block?
+- [ ] rework/further strip fmt.hpp
+	- [ ] since it is parsed to some type-erased list anyways, don't include the whole header
 	- [ ] constexpr string parsing
 - [ ] windows utf-8 output (see ny)
 - [ ] windows text style support
+- [ ] assert without error message
+- [ ] add real example and screenshot
--- sample.cpp
+++ sample.cpp
@@ -7,10 +7,13 @@
 
 using namespace dlg::literals;
 
+#define custom_trace(...) dlg_trace("sample"_project, "custom_trace"_scope, __VA_ARGS__)
+#define custom_assert(expr, ...) dlg_assert(expr, "sample"_project, "custom_assert"_scope, __VA_ARGS__)
+
 dlg::Logger* mySelector(dlg::Origin origin)
 {
-	if(origin.level == dlg::Level::trace || (origin.source.src[1] == "render"))
-		return nullptr;
+	// if(origin.level == dlg::Level::trace || (origin.source.src[1] == "render"))
+	// 	return nullptr;
 
 	return &dlg::defaultLogger;
 }
@@ -36,10 +39,14 @@ int main()
 	dlg_error("error {}", 5);
 	dlg_critical("render"_module, "critical {}", 6);
 
+	custom_trace("some custom trace");
+
 	std::cout << "just some normal output\n";
 
 	dlg_check("main_debug:1", {
 		dlg_assert(5 == 6, "assert failed");
+		custom_assert(1 == 1, "you should really not see this");
+		custom_assert(1 == 2, "uuuugh...");
 	});
 
 	dlg_log("Loggin is easy!");
